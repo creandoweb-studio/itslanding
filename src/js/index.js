c3d1db9b9bf342a9ed09f8c22fad34ddd61aeeec
Andres G <andresg2409@gmail.com>
2021-02-02 13:51:08 -0500

    responsive index - finalizado
    Vistas finalizadas

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import navigationOffCanvas from './components/navigation_off_canvas'
 import transparentHeader from './components/transparent_header'
-// import scrollProgress from './components/scroll_progress'
+import scrollProgress from './components/scroll_progress'
 // import bgImage from './components/bgImage'
 
 // bgImage()
-// scrollProgress()
+scrollProgress()
 navigationOffCanvas()
 transparentHeader()
 
